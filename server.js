392f5899745e5b6bd4657edf80cc661900e70bcc
raed-github <raed__saad@hotmail.com>
2023-04-01 19:05:28 +0400

    part-2 add a middleware to catch client requests and log the

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+require('dotenv').config()
+
 const express = require('express')
 
 const app = express()
 
+//middleware
+app.use((req,resp,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
 
 app.get('/',(req,res)=>{
     res.json(`application is running on port ${port}`)
 })
 
-const port = 4000
+const port = process.env.PORT
 
 app.listen(port,()=>{
     console.log(`app is running on port ${port}`)
