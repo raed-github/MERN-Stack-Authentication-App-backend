750a511b1fdbce906720f333f60a58624baedab6
raed-github <raed__saad@hotmail.com>
2023-04-04 20:42:35 +0400

    part-10 cache  tasks using memcache

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,11 +4,13 @@ const router = express.Router()
 
 const {getTasks,getTaskById,createTask,deleteTask,updateTask} = require('../controllers/tasksController')
 
+const {cache} = require('../util/caching')
+
 //GET all tasks
-router.get('',getTasks)
+router.get('',cache, getTasks)
 
 //GET a task by id
-router.get('/:id',getTaskById)
+router.get('/:id',cache, getTaskById)
 
 //POST a task
 router.post('',createTask)
---
+++ util/caching.js
@@ -0,0 +1,30 @@
+const Memcached = require('memcached');
+const memcached = new Memcached('localhost:11211');
+const {logger} = require('../util/logging')
+
+// Cache middleware
+const cache = (req, res, next) => {
+    const key = '__express__' + req.originalUrl || req.url;
+    memcached.get(key, (err, cachedData) => {
+    if (cachedData) {
+        logger.log('info', `loading cached response. key=${key}`);
+        res.send(cachedData);
+        return;
+    } else {
+        logger.log('info', `loading data from db`);
+        res.sendResponse = res.send;
+        res.send = (body) => {
+            memcached.set(key, body,3600, (err, data) => {
+            if (err || !data) {
+                logger.log('info', `${err}`);
+            }
+            logger.log('info', `caching response under key=${key}`);
+        });
+        res.sendResponse(body);
+        };
+        next();
+    }
+    });
+};
+
+module.exports = {cache}
